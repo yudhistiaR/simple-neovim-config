8b8f4a9b1d8096b6adcb66a254d25e7d0beca295
isKazumi_ <58545851+yudhistiaR@users.noreply.github.com>
2026-03-17 19:15:15 +0800

    Configure Telescope to ignore .git files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,23 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
     },
-    opts = {},
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "^%.git/", "node_modules/" },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+          no_ignore = false,
+          find_command = {
+            "rg",
+            "--files",
+            "--hidden",
+            "--glob",
+            "!.git/*",
+          },
+        },
+      },
+    },
   },
 }
