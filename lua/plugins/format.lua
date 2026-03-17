8894bb776a979efc1e5fb6b538f500a27306e928
yudhistia <yudhistia110@gmail.com>
2026-03-17 14:02:08 +0800

    fix(formatter): fix formatter time out

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,19 +1,33 @@
 return {
   {
     "stevearc/conform.nvim",
+    event = { "BufWritePre" },
     opts = {
       formatters_by_ft = {
-        javascript = { "prettierd", "prettier" },
-        javascriptreact = { "prettierd", "prettier"  },
-        typescript = { "prettierd", "prettier" },
-        typescriptreact = { "prettierd", "prettier" },
-        css = { "prettierd", "prettier"  },
-        html = { "prettierd", "prettier"  },
-        json = { "prettierd", "prettier"  },
-        yaml = { "prettierd", "prettier"  },
-        markdown = { "prettierd", "prettier" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        css = { "prettierd", "prettier", stop_after_first = true },
+        html = { "prettierd", "prettier", stop_after_first = true },
+        json = { "prettierd", "prettier", stop_after_first = true },
+        yaml = { "prettierd", "prettier", stop_after_first = true },
+        markdown = { "prettierd", "prettier", stop_after_first = true },
         lua = { "stylua" },
+        sh = { "shfmt" },
+        bash = { "shfmt" },
       },
+
+      format_on_save = function()
+        if vim.g.autoformat == false then
+          return
+        end
+
+        return {
+          timeout_ms = 3000,
+          lsp_format = "fallback",
+        }
+      end,
     },
   },
 }
